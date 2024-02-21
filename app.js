4306fdcbc0d25620189179a1765848d29c0a7988
mim chowdhury <umac3333@gmail.com>
2024-02-21 22:04:20 +0600

    function-add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 let totalSeat = 8;
 let count = 0;
+let grandAmount = 0;
+let totalAmount = 0;
 const allBtn = document.getElementsByClassName('btnText');
 
 for (const btn of allBtn) {
@@ -60,10 +62,14 @@ for (const btn of allBtn) {
 
 
         const totalPrice = document.getElementById('total-price').innerText;
-
         const convertedTotalPrice = parseInt(totalPrice);
-
         document.getElementById('total-price').innerText = convertedTotalPrice + 550;
+        console.log(typeof convertedTotalPrice)
+
+        totalAmount = convertedTotalPrice + 550;
+
+        // totalAmount += totalPrice;
+        console.log(totalAmount);
 
 
         const grandTotal = document.getElementById('grand-total').innerText;
@@ -71,7 +77,68 @@ for (const btn of allBtn) {
 
         document.getElementById('grand-total').innerText = convertedGrandTotal + 550;
 
-
     })
 
 }
+
+
+
+function continueFunction() {
+    window.location.reload();
+}
+
+function remove() {
+    const deleteCoupon = document.getElementById('coupon-sec');
+    deleteCoupon.classList.add('hidden');
+
+    const discountPrice = document.getElementById('discount-sec');
+    discountPrice.classList.remove('hidden');
+}
+
+
+const applyBtn = document.getElementById('apply-btn');
+applyBtn.addEventListener('click', function () {
+    // console.log('click')
+
+
+
+    document.getElementById('A1').disabled = true
+    document.getElementById('A2').disabled = true
+    document.getElementById('A3').disabled = true
+    document.getElementById('A4').disabled = true
+    document.getElementById('B1').disabled = true
+    document.getElementById('B2').disabled = true
+    document.getElementById('B3').disabled = true
+    document.getElementById('B4').disabled = true
+
+
+
+    const inputElement = document.getElementById('input-field').value;
+    const couponCode1 = inputElement.split(" ").join("").toUpperCase();
+    const couponCode2 = inputElement;
+
+    console.log(couponCode1);
+    console.log(couponCode2);
+    if (couponCode1 === "NEW15") {
+        const discountAmount = document.getElementById('discount-price');
+        const discount = totalAmount * 0.15;
+        discountAmount.innerText = discount;
+        const grand = document.getElementById('grand-total');
+        const final = totalAmount - discount;
+        grand.innerText = final;
+
+    }
+    else if (couponCode2 === "Couple 20") {
+        const discountAmount = document.getElementById('discount-price');
+        const discount = totalAmount * 0.2;
+        discountAmount.innerText = discount;
+        const grand = document.getElementById('grand-total');
+        const final = totalAmount - discount;
+        grand.innerText = final;
+
+
+    }
+    else {
+        alert('Invalid Coupon')
+    }
+})
